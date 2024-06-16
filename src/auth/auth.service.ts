5b7e0c5e55210628b6fefc9f797feb48f09f736f
phcbarros <paulohcbarros@outlook.com>
2024-06-16 16:50:13 -0300

    chore: create login

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { UsersService } from './users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(data: LoginDto) {
+    const user = await this.usersService.findOne(data.email);
+
+    if (!user || !bcrypt.compareSync(data.password, user.password)) {
+      throw new Error('Invalid credentials');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...result } = user;
+
+    return {
+      access_token: this.jwtService.sign(result),
+    };
+  }
+}
--- src/auth/users/users.service.ts
+++ src/auth/users/users.service.ts
@@ -31,4 +31,18 @@ export class UsersService {
   generateHashPassword(password: string) {
     return bcrypt.hashSync(password, 10);
   }
+
+  findOne(idOrEmail: number | string) {
+    return this.prismaService.user.findFirst({
+      where: {
+        ...(typeof idOrEmail === 'number'
+          ? {
+              id: idOrEmail,
+            }
+          : {
+              email: idOrEmail,
+            }),
+      },
+    });
+  }
 }
